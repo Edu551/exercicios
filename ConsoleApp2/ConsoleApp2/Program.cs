66a0abae9c96881d407f0181ec63872d5103456c
Eduardo Moraes Goncalves <eduardogoncalves18@gmail.com>
2022-08-04 15:18:43 -0300

    Atualização exercícios Beecreowd.

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -12,49 +12,79 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            //var arrayDados = Console.ReadLine().Split(' ').ToArray();
-
-            double mediaPonderada = MediaPonderada();
+            var arrayDados = Console.ReadLine().Split(' ').ToArray();
 
 
-            Console.WriteLine($"Media: {mediaPonderada}");
 
-            if (AprovaAluno(mediaPonderada))
-            {
-                Console.WriteLine("Aluno aprovado.");
-            }
-            else if (ReprovaAluno(mediaPonderada))
-            {
-                Console.WriteLine("Aluno reprovado.");
-            }
-            else
-            {
-                Console.WriteLine("Aluno em exame.");
-                
-                if (!ReprovaAluno(mediaPonderada))
-                {
-                    var notaExame = Convert.ToDouble(Console.ReadLine());
-
-                    Console.WriteLine($"Nota do exame: {notaExame}");
-
-                    double mediaFinal = (notaExame + mediaPonderada) / 2;
-
-                    if (ReprovaAluno(mediaFinal))
-                    {
-                        Console.WriteLine("Aluno repovado.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Aluno aprovado.");
-                        Console.WriteLine($"Media final: {mediaFinal}");
-                    }
-                }
-            }
-
-            
 
 
             Console.ReadLine();
+        }
+
+
+        public void BEE2434()
+        {
+            var arrayDados = Console.ReadLine().Split(' ').ToArray();
+
+            int dias = int.Parse(arrayDados[0]);
+            int saldoInicial = int.Parse(arrayDados[1]);
+            int menorValor = saldoInicial;
+
+            for (int i = 0; i < dias; i++)
+            {
+                int movimentacaoDoDia = int.Parse(Console.ReadLine());
+
+                int saldoFinal = saldoInicial + movimentacaoDoDia;
+
+                saldoInicial = saldoFinal;
+
+                if (saldoFinal < menorValor)
+                {
+                    menorValor = saldoFinal;
+                }
+                Console.WriteLine($"O menor valor é R${menorValor},00.");
+                Console.WriteLine($"O saldo do dia {i + 1} é R$ {saldoFinal},00.");
+            }
+        }
+
+        public void BEE1041()
+        {
+            var arrayDados = Console.ReadLine().Split(' ').ToArray();
+
+            double eixoX = double.Parse(arrayDados[0]);
+            double eixoY = Convert.ToDouble(arrayDados[1]);
+
+            if (eixoX == 0 && eixoY == 0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if (eixoX == 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if (eixoY == 0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+            else
+            {
+                if (eixoX > 0 && eixoY > 0)
+                {
+                    Console.WriteLine("Q1");
+                }
+                else if (eixoX < 0 && eixoY > 0)
+                {
+                    Console.WriteLine("Q2");
+                }
+                else if (eixoX < 0 && eixoY < 0)
+                {
+                    Console.WriteLine("Q3");
+                }
+                else
+                {
+                    Console.WriteLine("Q4");
+                }
+            }
         }
 
         public static double MediaPonderada()
@@ -104,10 +134,85 @@ namespace ConsoleApp2
                 return false;
             }
         }
+        public void URI1040()
+        {
+            double mediaPonderada = MediaPonderada();
 
 
+            Console.WriteLine($"Media: {mediaPonderada}");
 
+            if (AprovaAluno(mediaPonderada))
+            {
+                Console.WriteLine("Aluno aprovado.");
+            }
+            else if (ReprovaAluno(mediaPonderada))
+            {
+                Console.WriteLine("Aluno reprovado.");
+            }
+            else
+            {
+                Console.WriteLine("Aluno em exame.");
 
+                if (!ReprovaAluno(mediaPonderada))
+                {
+                    var notaExame = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine($"Nota do exame: {notaExame}");
+
+                    double mediaFinal = (notaExame + mediaPonderada) / 2;
+
+                    if (ReprovaAluno(mediaFinal))
+                    {
+                        Console.WriteLine("Aluno repovado.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Aluno aprovado.");
+                        Console.WriteLine($"Media final: {mediaFinal}");
+                    }
+                }
+            }
+        }
+        public void URI1040v2()
+        {
+            var arrayDados = Console.ReadLine().Split(' ').ToArray();
+
+            float a = float.Parse(arrayDados[0]);
+            float b = float.Parse(arrayDados[1]);
+            float c = float.Parse(arrayDados[2]);
+            float d = float.Parse(arrayDados[3]);
+
+            float media = (a * 2 + b * 3 + c * 4 + d * 1) / 10;
+
+            Console.WriteLine($"Media: {media:F1}");
+
+            if (media >= 7.0)
+            {
+                Console.WriteLine("Aluno aprovado");
+            }
+            else if (media >= 5.0 && media <= 6.9)
+            {
+                Console.WriteLine("Aluno em exame.");
+                float notaDoExame = float.Parse(Console.ReadLine());
+                Console.WriteLine($"Nota do exame: {notaDoExame:F1}");
+                float mediaFinal = (notaDoExame + media) / 2;
+
+                if (mediaFinal >= 5.0)
+                {
+                    Console.WriteLine("Aluno aprovado. ");
+                    Console.WriteLine($"Media final: {mediaFinal:F1}");
+                }
+                else
+                {
+                    Console.WriteLine("Aluno reprovado. ");
+                    Console.WriteLine($"Media final: {mediaFinal:F1}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Aluno reprovado.");
+            }
+        }
 
         public void URI1015()
         {
@@ -206,6 +311,18 @@ namespace ConsoleApp2
             double totalDaCompra = valorDoItem * quantidade;
 
             Console.WriteLine($"Total: R$ {totalDaCompra:N2}\n");
+        }
+
+        public void StringFormat()
+        {
+            object[] values = { 1603, 1794.68235, 15436.14 };
+            string result;
+            foreach (var value in values)
+            {
+                result = String.Format("{0,12:C4}   {0,12:E4}   {0,12:F4}   {0,12:N4}  {1,12:P4}\n",
+                                       Convert.ToDouble(value), Convert.ToDouble(value) / 10000);
+                Console.WriteLine(result);
+            }
         }
     }   
 }
